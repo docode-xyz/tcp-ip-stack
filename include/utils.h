1d7140869dd36789d94d41c09f7b3bdf55dcb15c
Valery Ivanov <ivalery111@gmail.com>
2020-07-16 21:39:19 +0300

    added macro to test if the mac is broadcast

--- include/utils.h
+++ include/utils.h
@@ -2,6 +2,11 @@
 #define UTILS_H
 
 #include <stdint.h>
+
+#define IS_BROADCAST_MAC(mac)                                                  \
+  (mac[0] == 0xFF && mac[1] == 0xFF && mac[2] == 0xFF && mac[3] == 0xFF &&     \
+   mac[4] == 0xFF && mac[5] == 0xFF)
+
 void util_apply_mask(const char *prefix, const char mask, char *str_prefix);
 
 void util_l2_fill_mac_broadcast(uint8_t *mac_arr);
